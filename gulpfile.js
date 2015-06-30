fb3911ac0e24f02a1a51de1977daee6a42572045
Jeff Hanson <jshanson7@gmail.com>
2015-06-30 14:26:15 -0700

    clean up gulpfile, merge streams

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 'use strict';
 
 var gulp = require('gulp');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var gulpif = require('gulp-if');
+var mergeStream = require('merge-stream');
 var argv = require('yargs').argv;
 var watchify = require('watchify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var gutil = require('gulp-util');
-var rename = require('gulp-rename');
 var _ = require('lodash');
 var isProduction = !!argv.production;
 
-var bundleMaps = [
+gulp.task('bundle', bundleAll);
+gulp.task('watch', watchAll);
+gulp.task('default', ['bundle', 'watch']);
+
+var bundleConfigs = [
   {
     'entries': ['./src/sheet.js'],
     'dist': './dist',
@@ -25,45 +32,38 @@ var bundleMaps = [
   }
 ];
 
-var bundlers = bundleMaps.map(function (bundleMap) {
+var bundlers = bundleConfigs.map(function (bundleConfig) {
   var b = browserify(_.assign({}, watchify.args, {
-    entries: bundleMap.entries,
+    entries: bundleConfig.entries,
     debug: true
   }));
-  var w = watchify(b);
+  var watchifyBundler = watchify(b);
   return {
-    'watch': _.partial(watch, w),
-    'bundle': _.partial(bundle, w, bundleMap)
+    watch: function () {
+      return watchifyBundler
+        .on('update', bundleAll)
+        .on('log', gutil.log)
+        .bundle();
+    },
+    bundle: function () {
+      return watchifyBundler
+        .bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source(bundleConfig.file))
+        .pipe(buffer())
+        .pipe(gulp.dest(bundleConfig.dist))
+        .pipe(gulpif(isProduction, uglify()))
+        .pipe(gulpif(isProduction, rename({suffix: '.min'})))
+        .pipe(gulpif(isProduction, gulp.dest(bundleConfig.dist)));
+    }
   }
 });
 
-function watch (bundler) {
-  bundler.on('update', bundleAll);
-  bundler.on('log', gutil.log);
+function bundleAll() {
+  return mergeStream.apply(gulp, _.invoke(bundlers, 'bundle'));
 }
 
-function bundle (bundler, bundleMap) {
-  bundler.bundle()
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source(bundleMap.file))
-    .pipe(buffer())
-    .pipe(gulp.dest(bundleMap.dist));
-
-  if (isProduction) {
-    bundler.bundle()
-      .on('error', gutil.log.bind(gutil, 'Uglify Error'))
-      .pipe(source(bundleMap.file))
-      .pipe(buffer())
-      .pipe(uglify())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(bundleMap.dist));
-  }
+function watchAll() {
+  return mergeStream.apply(gulp, _.invoke(bundlers, 'watch'));
 }
 
-function watchAll () { _.invoke(bundlers, 'watch'); }
-function bundleAll () { _.invoke(bundlers, 'bundle'); }
-
-gulp.task('default', function (done) {
-  bundleAll();
-  watchAll();
-});
